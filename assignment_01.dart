835addf882fd0ae973590cfdc0ff3ee0ee2bb3c2
salmanshaikh02 <125751489+salmanshaikh02@users.noreply.github.com>
2024-06-30 15:31:35 +0500

    Add files via upload

---
+++ assignment_01.dart
@@ -0,0 +1,101 @@
+import 'dart:ffi';
+
+void main(List<String> arguments) {
+
+  // Question 1: Create two integer variables length and breadth and assign values then check if they   are square values or rectangle values.
+  // ie: if both values are equal then it's square otherwise rectangle.
+
+      // int length = 40;
+      // int breadth = 40;
+
+      // if (length == breadth){
+      //   print("It's Square!");
+      // }
+
+      // else{
+      //   print("It's Rectangle!");
+      // }
+
+  // Question 2: Take two variables and store age then using if/else condition to determine oldest and youngest among them.
+
+      // int age1 = 15;
+      // int age2 = 34;
+
+      // if(age1 > age2){
+      // print("Oldest is $age1 and youngest is $age2");
+      // }
+      // else if(age2 > age1){
+      // print("Oldest is $age2 and youngest is $age1");
+      // }
+
+  // Question 3: A student will not be allowed to sit in exam if his/her attendance is less than 75%. Create integer variables and assign value:
+  // Number of classes held = 16,
+  // Number of classes attended = 10,
+  // and print percentage of class attended.
+  // Is student is allowed to sit in exam or not?
+
+      // int classesHeld = 16;
+      // int classesAttend = 10;
+      // double percentage = (classesAttend / classesHeld) * 100;
+
+      // if(percentage >= 75){
+      //   print("Allowed to sit in exam, attendance is: $percentage%");
+      // }
+      // else{
+      //   print("NOT allowed to sit in exam, attendance is: $percentage%");
+      // }
+
+  // Question 4: Write a program to convert Celsius to Fahrenheit .
+  // i.e: Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+
+      // double celsius = 44.9;
+      // double fahrenheit = (celsius * 9/5) + 32;
+
+      // print("$celsius°C is equal to $fahrenheit°F");
+
+  // Question 5: Write a program to read temperature in centigrade and display a suitable
+  // message according to temperature:
+  // You have num variable temperature = 42;
+  // Now print the message according to temperature:
+  // temp < 0 then Freezing weather
+  // temp 0-10 then Very Cold weather
+  // temp 10-20 then Cold weather
+  // temp 20-30 then Normal in Temp
+  // temp 30-40 then Its Hot
+  // temp >=40 then Its Very Hot
+
+      // double temperature = 44.5;
+
+      // if(temperature >= 40){
+      //   print("It's very hot!");
+      // }
+      // else if(temperature > 30 && temperature < 40){
+      //   print("It's Hot!");
+      // }
+      // else if(temperature > 20 && temperature < 30){
+      //   print("Normal Temperature.");
+      // }
+      // else if(temperature > 10 && temperature < 20){
+      //   print("Cold Weather!");
+      // }
+      // else if(temperature > 0 && temperature < 10){
+      //   print("Very Cold weather");
+      // }
+      // else{
+      //   print("Freezing Temperature.");
+      // }
+      
+  // Question 6: Write a program to check whether an alphabet is a vowel or consonant.
+
+      // String alphabet = "a";
+
+      // if(alphabet == "a" || alphabet == "e" || alphabet == "i" || alphabet == "o" || alphabet == "u"){
+      //   print("$alphabet is vowel");
+      // }
+      // else{
+      //   print("$alphabet is consonant");
+      // }
+
+  // Question 8: 
+  
+}
